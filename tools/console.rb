49198cac00f2fa5743b7b9632f22d76596999ff7
bossteve1 <stephen.nyabuto@student.moringaschool.com>
2022-10-31 00:46:40 +0300

    add test code

--- tools/console.rb
+++ tools/console.rb
@@ -6,7 +6,17 @@ end
 
 
 ###  WRITE YOUR TEST CODE HERE ###
-
+author1 =Author.new("chamwada")
+citizen=Magazine.new("citizen", "nipashe")
+ntv=Magazine.new("ntv","nipashe")
+ktn=Magazine.new("ktn","uhalifu")
+leo=Article.new(chamwada,citizen,"ungwana")
+kesho=Article.new(chamwada,citizen,"raia")
+chamwada.add_article(citizen,"kioo")
+chamwada.add_article(citizen,"usanii")
+chamwada.add_article(citizen,"siasa")
+chamwada.add_article(ntv,"wizi")
+chamwada.add_article(ktn,"sanaa")
 
 
 
