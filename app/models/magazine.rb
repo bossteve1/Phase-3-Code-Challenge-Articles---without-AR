6ef8e245b6c00de0c83eeb02e175cc7f070b930d
bossteve1 <stephen.nyabuto@student.moringaschool.com>
2022-10-30 23:22:09 +0300

    add articlee titles

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -13,15 +13,24 @@ class Magazine
     @@all
   end
 
+  # helper method
+  def mag_articles
+    Article.all.filter{|article|article.magazine.name==@name}
+  end
+
   def contributors
-    Article.all.filter{|article|article.magazine.name==@name}.map{|article|article.author}.uniq
+    mag_articles.map{|article|article.author}.uniq
   end
 
   def self.find_by_name(name)
     Magazine.all.find{|magazine|magazine.name==name}
   end
 
+  def article_titles
+    mag_articles.map{|article|article.title}
+  end
 
+  
   
 
 
