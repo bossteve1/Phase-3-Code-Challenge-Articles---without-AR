7ac5313ebe01acf24faa570e8f54e2c62630ab18
bossteve1 <stephen.nyabuto@student.moringaschool.com>
2022-10-30 19:03:48 +0300

    article code

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,20 @@
 class Article
-    
+    attr_reader:magazine, :title
+     
+    @@all = []
+    def initialize(author, magazine, title)
+        @author = author
+        @magazine = magazine
+        @title = title
+    end
+
+    def self.all
+        @@all
+    end
+
+    def author
+        @author.name
+    end
 
 
 end
