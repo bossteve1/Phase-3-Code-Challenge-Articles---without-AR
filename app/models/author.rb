a60436005d385b30270faa9e652233dae26a65f9
bossteve1 <stephen.nyabuto@student.moringaschool.com>
2022-10-30 19:03:55 +0300

    author code

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,10 +1,20 @@
 class Author
-  attr_accessor :name
+  attr_reader :name
 
 
   def initialize(name)
     @name = name
   
+  end
+
+  def articles
+    Article.all.filter{|article|
+    article.author==@name}
+  end
+
+  def magazines
+    articles.map{|article|article.magazine}.uniq
+
   end
 
 
